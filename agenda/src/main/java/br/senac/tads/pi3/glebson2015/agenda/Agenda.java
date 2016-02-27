f84a4082952c29fbef3201469fff1fb191e52c83
glebson.lsilva1 <glebson.lsilva1@CAS521257W0736.senacsp.edu.br>
2016-02-26 22:35:48 -0300

    Criamos um método de conexão com o banco.

--- agenda/src/main/java/br/senac/tads/pi3/glebson2015/agenda/Agenda.java
+++ agenda/src/main/java/br/senac/tads/pi3/glebson2015/agenda/Agenda.java
@@ -5,12 +5,30 @@
  */
 package br.senac.tads.pi3.glebson2015.agenda;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author glebson.lsilva1
  */
 public class Agenda {
-    
-    
-    
+
+    private Connection obterConexao() throws SQLException, ClassNotFoundException {
+
+        Connection conn = null;
+        //Passo: Registrar drive JDBC
+        Class.forName("org.apche.derby.jdbc.ClientDataSource");
+
+        //Passo 2: Abrir a conexão
+        conn = DriverManager.getConnection(
+                "jdbc:derby://localhost:1527/agendabd;SecurityMechanism=3",
+                "app", //Usuário
+                "app" //Senha
+        );
+        return conn;
+
+    }
+
 }
