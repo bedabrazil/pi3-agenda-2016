11015ffd9a78538ce88ba09c8626a2b6a139c0cd
developer <developer@192.168.1.148>
2016-02-28 09:31:57 -0300

    separada o metodo de conexao para uma classe de conexao; inserido o metodo de listar pessoas;

---
+++ agenda/src/main/java/br/senac/tads/pi3/glebson2015/agenda/Conexao.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads.pi3.glebson2015.agenda;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author marcio.soares <marcio@mail.com>
+ */
+public class Conexao {
+        public Connection obterConexao() throws SQLException, ClassNotFoundException {
+
+        Connection conn = null;
+        //Passo: Registrar drive JDBC
+        Class.forName("org.apche.derby.jdbc.ClientDataSource");
+
+        //Passo 2: Abrir a conexão
+        conn = DriverManager.getConnection(
+                "jdbc:derby://localhost:1527/agendabd;SecurityMechanism=3",
+                "app", //Usuário
+                "app" //Senha
+        );
+        return conn;
+    }  
+}
