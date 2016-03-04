9b8ece2c9baa767cf698087816e509d1e5f25b57
glebson2015 <glebson.lourenco@gmail.com>
2016-03-04 00:06:59 -0300

    Update Agenda.java
    Foi criado um menu na classe Agenda utilizando um do/while e condicionais if

--- agenda/src/main/java/br/senac/tads/pi3/glebson2015/agenda/Agenda.java
+++ agenda/src/main/java/br/senac/tads/pi3/glebson2015/agenda/Agenda.java
@@ -11,5 +11,48 @@ package br.senac.tads.pi3.glebson2015.agenda;
  */
 public class Agenda {
     
-    
+    Pessoa pessoa = new Pessoa();
+
+        //Variável de opção
+        int opcao;
+
+        //Scanner - Entrada de dados do usuário
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("******************************");
+        System.out.println("*     AGENDA DE CONTATOS     *");
+        System.out.println("******************************");
+
+        //Do while - Controla o menu da agenda por meio de condicionais if
+        do {
+
+            System.out.println("Escolha uma das opçõesdo menu abaixo: \n1 - Cadastrar Contato"
+                    + "\n2 - Alterar Contato\n3 - Mostrar Contato\n4 - Excluir Contatos\n5 - Sair da Agenda");
+            opcao = sc.nextInt();
+
+            if (opcao == 1) {
+                
+                
+            }
+            if (opcao == 2) {
+
+            }
+            if (opcao == 3) {
+
+            }
+            if (opcao == 4) {
+
+            }
+            if (opcao == 5) {
+
+                System.out.println("Sistema de agenda de contato finalizado!!");
+            }
+
+        } while (opcao != 5);
+
+    }
+
 }
+    
+    
+
